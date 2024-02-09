5d7a1fd76e494b40170b60301e628fbc1da450bd
Passarin <almeida.andre@gmail.com>
2024-02-09 11:16:50 -0300

    versão sp

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-#![allow(unused_variables)]
-
-use std::env;
-use std::sync::Arc;
-use std::time::SystemTime;
-use axum::{routing::get, Router, Json};
-use axum::extract::{Path, State};
-use axum::http::StatusCode;
-use axum::routing::post;
+use std::{env, sync::Arc, time::SystemTime};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    routing::{get, post},
+    Router, Json,
+};
 use axum_route_error::RouteError;
 use axum_valid::Valid;
-use deadpool_postgres::{GenericClient};
-use deadpool_postgres::Runtime::Tokio1;
-use deadpool_postgres::tokio_postgres::{NoTls, Row};
+use deadpool_postgres::{tokio_postgres::NoTls, GenericClient, Runtime::Tokio1, tokio_postgres::Row};
 use tokio::net::TcpListener;
 use validator::Validate;
 
@@ -19,7 +15,15 @@ pub struct AppState {
     pub pg_pool: deadpool_postgres::Pool
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, Validate)]
+pub struct TransacaoPayload {
+    pub valor: i32,
+    pub tipo: String,
+    #[validate(length(min = 1, max = 10))]
+    pub descricao: String
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct SaldoLimite {
     pub limite: i32,
     pub saldo: i32
@@ -34,13 +38,13 @@ impl From<&Row> for SaldoLimite {
     }
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct Extrato {
     pub saldo: Saldo,
     pub ultimas_transacoes: Vec<Transacao>
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct Saldo {
     pub total: i32,
     pub data_extrato: String,
@@ -60,15 +64,7 @@ impl From<&Row> for Saldo {
     }
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, Validate)]
-pub struct TransacaoPayload {
-    pub valor: i32,
-    pub tipo: String,
-    #[validate(length(min = 1, max = 10))]
-    pub descricao: String
-}
-
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct Transacao {
     pub valor: i32,
     pub tipo: String,
@@ -95,9 +91,8 @@ async fn main() -> Result<(), anyhow::Error>{
     dotenv::from_path(".env.local").ok().unwrap_or_else(|| {dotenv::dotenv().ok();});
     let cfg = Config::from_env()?;
 
-    let pg_pool = cfg.pg.create_pool(Some(Tokio1), NoTls)?;
     let app_state = Arc::new( AppState {
-        pg_pool
+        pg_pool: cfg.pg.create_pool(Some(Tokio1), NoTls)?
     });
 
     let app = Router::new()
@@ -113,13 +108,12 @@ async fn main() -> Result<(), anyhow::Error>{
     Ok(())
 }
 
-pub async fn post_transacoes(
-    State(state): State<Arc<AppState>>,
+pub async fn post_transacoes(State(state): State<Arc<AppState>>,
     Path(id): Path<i32>,
     Valid(Json(transacao)): Valid<Json<TransacaoPayload>>
 ) -> Result<Json<SaldoLimite>, RouteError>   {
 
-    if id > 5 { return Err(RouteError::new_not_found()); }
+    if id > 5 { return Err(RouteError::new_not_found()) }
 
     let valor = match transacao.tipo.as_str() {
         "c" => transacao.valor,
@@ -127,69 +121,44 @@ pub async fn post_transacoes(
         _ => return Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY))
     };
 
-    let trans = state.pg_pool.get().await.expect("Failed to get connection from pool");
-    //let trans = conn.transaction().await?;
+    let conn = state.pg_pool.get().await.expect("Failed to get connection from pool");
 
-    let sql_prep_update = trans.prepare_cached(
-        r#"UPDATE cliente
-                  SET saldo = saldo + $1
-                  WHERE id = $2 AND saldo + $3 >= - limite
-                  RETURNING saldo, limite"#
-    ).await.expect("Failed to prepare sql_prep_update");
+    let result_transacionar = conn.query(
+        r#"CALL TRANSACIONAR($1, $2, $3, $4);"#, &[&id, &valor, &transacao.tipo, &transacao.descricao])
+        .await.expect("Erro ao executar transacionar");
 
-    let row = trans.query(&sql_prep_update,
-        &[&valor, &id, &valor])
-        .await.expect("Failed to execute sql_prep_update");
+    match result_transacionar[0].get::<_, Option<i32>>(0) {
+        Some(_) => Ok(Json(SaldoLimite::from(&result_transacionar[0]))),
+        None => Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY))
+    }
 
-    if row.is_empty() { return Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY)); }
-
-    //trans.commit().await.expect("Failed to commit transaction");
-
-    let sql_prep_trans = trans.prepare_cached(
-        r#"INSERT INTO transacao (cliente_id, valor, tipo, descricao) VALUES ($1, $2, $3, $4);"#)
-        .await.expect("Failed to prepare sql_prep_trans");
-
-    trans.query(&sql_prep_trans,
-        &[&id, &transacao.valor, &transacao.tipo, &transacao.descricao])
-        .await.expect("Failed to execute sql_prep_trans");
-
-    Ok(Json(SaldoLimite::from(&row[0])))
 }
 
-pub async fn get_extrato(State(state): State<Arc<AppState>>, Path(id): Path<i32>) -> Result<Json<Extrato>, RouteError> {
+pub async fn get_extrato(
+    State(state): State<Arc<AppState>>,
+    Path(id): Path<i32>
+) -> Result<Json<Extrato>, RouteError> {
 
     if id > 5 { return Err(RouteError::new_not_found()); }
 
-    let conn = state.pg_pool.get().await.expect("Failed to get connection from pool");
+    let conn = state.pg_pool.get().await?;
 
-    let sql_prep_cliente = conn
-        .prepare_cached(r#"SELECT C.saldo, C.limite FROM cliente C WHERE C.id = $1;"#)
-        .await.expect("Failed to prepare statement");
+    let s_cliente = conn
+        .prepare_cached(r#"SELECT C.saldo, C.limite FROM cliente C WHERE C.id = $1;"#).await?;
+    let cliente = conn.query(&s_cliente, &[&id]).await?;
 
-    let row = conn
-        .query(&sql_prep_cliente, &[&id])
-        .await.expect("Failed to execute sql_prep_cliente");
+    if cliente.is_empty() { return Err(RouteError::new_not_found()); }
 
-    if row.is_empty() { return Err(RouteError::new_not_found()); }
-
-    let saldo = Saldo::from(&row[0]);
-
-    let sql_pre_trans = conn.prepare_cached(
+    let s_transacoes = conn.prepare_cached(
         r#"SELECT T.valor, T.tipo, T.descricao, T.realizada_em
         FROM transacao T WHERE T.cliente_id = $1
-        ORDER BY T.realizada_em DESC LIMIT 10;"#
-    ).await.expect("Failed to prepare statement");
+        ORDER BY T.realizada_em DESC LIMIT 10;"#).await?;
+    let transacoes = conn.query(&s_transacoes, &[&id]).await?;
 
-    let rows = conn
-        .query(&sql_pre_trans, &[&id])
-        .await.expect("Failed to execute sql_pre_trans");
-
-    let ultimas_transacoes = rows
-        .iter()
-        .map(|row| Transacao::from(row))
-        .collect();
-
-    Ok(Json(Extrato { saldo, ultimas_transacoes }))
+    Ok(Json(Extrato {
+        saldo: Saldo::from(&cliente[0]),
+        ultimas_transacoes: transacoes.iter().map(|row| Transacao::from(row)).collect() })
+    )
 }
 
 #[derive(Debug, serde::Deserialize)]
