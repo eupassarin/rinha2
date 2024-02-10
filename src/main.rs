d5b96836f9f7cd443157ec312f9d0fcfc3575099
Passarin <almeida.andre@gmail.com>
2024-02-10 00:56:53 -0300

    hacks

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,90 @@ pub struct AppState {
     pub pg_pool: deadpool_postgres::Pool
 }
 
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error>{
+    dotenv::from_path(".env.local").ok().unwrap_or_else(|| {dotenv::dotenv().ok();});
+
+    let cfg = Config::from_env()?;
+    let app_state = Arc::new( AppState { pg_pool: cfg.pg.create_pool(Some(Tokio1), NoTls)? });
+
+    let app = Router::new()
+        .route("/clientes/:id/transacoes", post(post_transacoes))
+        .route("/clientes/:id/extrato", get(get_extrato))
+        .with_state(app_state);
+
+    let http_port = env::var("HTTP_PORT").unwrap_or_else(|_| "80".to_string());
+    let listener = TcpListener::bind(format!("0.0.0.0:{http_port}")).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+pub async fn post_transacoes(State(state): State<Arc<AppState>>,
+    Path(id): Path<i32>,
+    Valid(Json(transacao)): Valid<Json<TransacaoPayload>>
+) -> Result<Json<SaldoLimite>, RouteError>   {
+
+    if id > 5 { return Err(RouteError::new_not_found()) }
+
+    let valor = match transacao.tipo.as_str() {
+        "c" => transacao.valor,
+        "d" => -transacao.valor,
+        _ => return Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY))
+    };
+
+    let conn = state.pg_pool.get().await?;
+    let result_transacionar = conn.query(
+        r#"CALL TRANSACIONAR($1, $2, $3, $4);"#, &[&id, &valor, &transacao.tipo, &transacao.descricao])
+        .await?;
+
+    match result_transacionar[0].get::<_, Option<i32>>(0) {
+        Some(_) => Ok(Json(SaldoLimite::from(&result_transacionar[0]))),
+        None => Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY))
+    }
+
+}
+
+pub async fn get_extrato(
+    State(state): State<Arc<AppState>>,
+    Path(id): Path<i32>
+) -> Result<Json<Extrato>, RouteError> {
+
+    if id > 5 { return Err(RouteError::new_not_found()); }
+
+    let conn = state.pg_pool.get().await?;
+    let cliente = conn.query(&conn.prepare_cached(
+        r#"SELECT C.saldo, C.limite FROM cliente C WHERE C.id = $1;"#).await?, &[&id])
+    .await?;
+
+    if cliente.is_empty() { return Err(RouteError::new_not_found()); }
+
+    let transacoes = conn.query(&conn.prepare_cached(
+        r#"SELECT T.valor, T.tipo, T.descricao, T.realizada_em
+        FROM transacao T WHERE T.cliente_id = $1
+        ORDER BY T.realizada_em DESC LIMIT 10;"#).await?, &[&id]).await?;
+
+    Ok(Json(Extrato {
+        saldo: Saldo::from(&cliente[0]),
+        ultimas_transacoes: transacoes.iter().map(|row| Transacao::from(row)).collect() })
+    )
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct Config {
+    pub pg: deadpool_postgres::Config
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, config::ConfigError> {
+        config::Config::builder()
+            .add_source(config::Environment::default().separator("__"))
+            .build()?
+            .try_deserialize()
+    }
+}
+
 #[derive(serde::Serialize, serde::Deserialize, Validate)]
 pub struct TransacaoPayload {
     pub valor: i32,
@@ -82,94 +166,5 @@ impl From<&Row> for Transacao {
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap().as_secs() as i64
         }
-    }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error>{
-
-    dotenv::from_path(".env.local").ok().unwrap_or_else(|| {dotenv::dotenv().ok();});
-    let cfg = Config::from_env()?;
-
-    let app_state = Arc::new( AppState {
-        pg_pool: cfg.pg.create_pool(Some(Tokio1), NoTls)?
-    });
-
-    let app = Router::new()
-        .route("/clientes/:id/transacoes", post(post_transacoes))
-        .route("/clientes/:id/extrato", get(get_extrato))
-        .with_state(app_state);
-
-    let http_port = env::var("HTTP_PORT").unwrap_or_else(|_| "80".to_string());
-    let listener = TcpListener::bind(format!("0.0.0.0:{http_port}")).await?;
-
-    axum::serve(listener, app).await?;
-
-    Ok(())
-}
-
-pub async fn post_transacoes(State(state): State<Arc<AppState>>,
-    Path(id): Path<i32>,
-    Valid(Json(transacao)): Valid<Json<TransacaoPayload>>
-) -> Result<Json<SaldoLimite>, RouteError>   {
-
-    if id > 5 { return Err(RouteError::new_not_found()) }
-
-    let valor = match transacao.tipo.as_str() {
-        "c" => transacao.valor,
-        "d" => -transacao.valor,
-        _ => return Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY))
-    };
-
-    let conn = state.pg_pool.get().await.expect("Failed to get connection from pool");
-
-    let result_transacionar = conn.query(
-        r#"CALL TRANSACIONAR($1, $2, $3, $4);"#, &[&id, &valor, &transacao.tipo, &transacao.descricao])
-        .await.expect("Erro ao executar transacionar");
-
-    match result_transacionar[0].get::<_, Option<i32>>(0) {
-        Some(_) => Ok(Json(SaldoLimite::from(&result_transacionar[0]))),
-        None => Err(RouteError::new_from_status(StatusCode::UNPROCESSABLE_ENTITY))
-    }
-
-}
-
-pub async fn get_extrato(
-    State(state): State<Arc<AppState>>,
-    Path(id): Path<i32>
-) -> Result<Json<Extrato>, RouteError> {
-
-    if id > 5 { return Err(RouteError::new_not_found()); }
-
-    let conn = state.pg_pool.get().await?;
-
-    let cliente = conn.query(&conn.prepare_cached(
-        r#"SELECT C.saldo, C.limite FROM cliente C WHERE C.id = $1;"#).await?, &[&id])
-    .await?;
-
-    if cliente.is_empty() { return Err(RouteError::new_not_found()); }
-
-    let transacoes = conn.query(&conn.prepare_cached(
-        r#"SELECT T.valor, T.tipo, T.descricao, T.realizada_em
-        FROM transacao T WHERE T.cliente_id = $1
-        ORDER BY T.realizada_em DESC LIMIT 10;"#).await?, &[&id]).await?;
-
-    Ok(Json(Extrato {
-        saldo: Saldo::from(&cliente[0]),
-        ultimas_transacoes: transacoes.iter().map(|row| Transacao::from(row)).collect() })
-    )
-}
-
-#[derive(Debug, serde::Deserialize)]
-pub struct Config {
-    pub pg: deadpool_postgres::Config
-}
-
-impl Config {
-    pub fn from_env() -> Result<Self, config::ConfigError> {
-        config::Config::builder()
-            .add_source(config::Environment::default().separator("__"))
-            .build()?
-            .try_deserialize()
     }
 }
