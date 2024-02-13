ac591ea998f65662007b57ac4836cbeda642067b
Passarin <almeida.andre@gmail.com>
2024-02-13 08:32:49 -0300

    hacks

--- init.sql
+++ init.sql
@@ -1,3 +1,18 @@
+-- DATABASE
+ALTER DATABASE RINHA SET SYNCHRONOUS_COMMIT=OFF;
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+SET default_tablespace = '';
+SET default_table_access_method = heap;
+
 -- CLIENTE
 CREATE UNLOGGED TABLE IF NOT EXISTS CLIENTE (
      ID SMALLINT,
@@ -63,17 +78,6 @@ BEGIN
 END;
 $$;
 
--- DATABASE
-ALTER DATABASE RINHA SET SYNCHRONOUS_COMMIT=OFF;
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-SET default_tablespace = '';
-SET default_table_access_method = heap;
+
+
