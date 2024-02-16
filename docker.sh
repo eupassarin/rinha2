17e01eae9cdf93e0a9a185e8ec437568ab8be116
Passarin <almeida.andre@gmail.com>
2024-02-16 10:38:35 -0300

    hacks

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose down --volumes
-docker-compose build
-docker-compose up
+docker-compose -f docker-compose.local.yml down  --volumes
+docker-compose -f docker-compose.local.yml build
+docker-compose -f docker-compose.local.yml up
