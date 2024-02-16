92e072219182a6eaea9e5e3084a72cca6d64e009
Passarin <almeida.andre@gmail.com>
2024-02-15 23:31:38 -0300

    hacks

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,12 @@ use deadpool_postgres::{tokio_postgres::NoTls, GenericClient, Runtime::Tokio1, P
 use serde_json::{from_slice, to_string};
 use tokio::{net::TcpListener, task, try_join};
 
-pub struct AppState {
-    pub pg_pool: Pool,
-    pub limites: Vec<i32>,
-}
-
 #[tokio::main]
 async fn main() {
     dotenv::from_path(".env.local").ok().unwrap_or_else(|| {dotenv::dotenv().ok();});
 
     let cfg = Config::from_env().unwrap();
-    let pg_pool = cfg.pg.create_pool(Some(Tokio1), NoTls).unwrap();
-
-    let conn = pg_pool.get().await.unwrap();
-    let limites = conn.query(SELECT_LIMITE, &[])
-        .await.unwrap().iter().map(|row| row.get(0)).collect();
-
-    warmup(&pg_pool).await;
-
-    let app_state = Arc::new(AppState{ pg_pool, limites });
+    let app_state = Arc::new(cfg.pg.create_pool(Some(Tokio1), NoTls).unwrap());
 
     let app = Router::new()
         .route("/clientes/:id/transacoes", post(post_transacoes))
@@ -33,20 +20,9 @@ async fn main() {
     let http_port = env::var("HTTP_PORT").unwrap_or_else(|_| "80".to_string());
     let listener = TcpListener::bind(format!("0.0.0.0:{http_port}")).await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
 }
 
-async fn warmup(pg_pool: &Pool) {
-    for _ in 0..500 {
-        let conn = pg_pool.get().await.unwrap();
-        task::spawn(async move {
-            let _ = conn.query(SELECT_LIMITE, &[]).await.unwrap();
-        });
-    }
-    println!("{:?}/{:?}", pg_pool.status().size, pg_pool.status().available);
-}
-
-pub async fn post_transacoes(State(state): State<Arc<AppState>>, Path(id): Path<i16>, transacao: body::Bytes)
+pub async fn post_transacoes(State(pg_pool): State<Arc<Pool>>, Path(id): Path<i16>, transacao: body::Bytes)
                              -> impl IntoResponse
 {
     if id > 5 { return (StatusCode::NOT_FOUND, String::new()) }
@@ -56,7 +32,7 @@ pub async fn post_transacoes(State(state): State<Arc<AppState>>, Path(id): Path<
         _ => return (StatusCode::UNPROCESSABLE_ENTITY, String::new())
     };
 
-    let conn = state.pg_pool.get().await.unwrap();
+    let conn = pg_pool.get().await.unwrap();
     match conn.query_one(UPDATE_SALDO, &[&(if t.tipo == 'd' { -t.valor } else { t.valor }), &id]).await {
         Ok(result) => {
             task::spawn(async move {
@@ -71,7 +47,7 @@ pub async fn post_transacoes(State(state): State<Arc<AppState>>, Path(id): Path<
                 StatusCode::OK,
                 to_string(&SaldoLimite {
                     saldo: Ok::<i32, Error>(result.get(0)).unwrap(),
-                    limite: state.limites[(id - 1) as usize]
+                    limite: [1000_00, 800_00,10000_00,100000_00,5000_00][(id - 1) as usize]
                 }).unwrap()
             )
         },
@@ -79,19 +55,15 @@ pub async fn post_transacoes(State(state): State<Arc<AppState>>, Path(id): Path<
     }
 }
 
-pub async fn get_extrato(State(state): State<Arc<AppState>>, Path(id): Path<i16>) -> impl IntoResponse  {
+pub async fn get_extrato(State(pg_pool): State<Arc<Pool>>, Path(id): Path<i16>) -> impl IntoResponse  {
 
     if id > 5 { return (StatusCode::NOT_FOUND, String::new()) }
 
-    let conn = state.pg_pool.get().await.unwrap();
+    let conn = pg_pool.get().await.unwrap();
 
     let (cliente, transacoes) = try_join!(
-        async {
-            conn.query_one(&conn.prepare_cached(SELECT_SALDO).await.unwrap(), &[&id]).await
-        },
-        async {
-            conn.query(&conn.prepare_cached(SELECT_TRANSACAO).await.unwrap(), &[&id]).await
-        }
+        async {conn.query_one(&conn.prepare_cached(SELECT_SALDO).await.unwrap(), &[&id]).await},
+        async {conn.query(&conn.prepare_cached(SELECT_TRANSACAO).await.unwrap(), &[&id]).await}
     ).unwrap();
 
     (StatusCode::OK, to_string(
@@ -99,7 +71,7 @@ pub async fn get_extrato(State(state): State<Arc<AppState>>, Path(id): Path<i16>
                 saldo: Saldo{
                     total: cliente.get(0),
                     data_extrato: now_monotonic(),
-                    limite: state.limites[(id-1) as usize]
+                    limite: [1000_00, 800_00,10000_00,100000_00,5000_00][(id-1) as usize]
             },
             ultimas_transacoes: transacoes.iter().map(|row| Transacao {
                 valor: row.get(0),
@@ -115,12 +87,9 @@ const UPDATE_SALDO: &str = "UPDATE C SET S = S + $1 WHERE I = $2 RETURNING S";
 const INSERT_TRANSACTION: &str = "INSERT INTO T(I, V, P, D) VALUES($1, $2, $3, $4)";
 const SELECT_TRANSACAO: &str = "SELECT V, P, D, R FROM T WHERE I = $1 ORDER BY R DESC LIMIT 10";
 const SELECT_SALDO: &str = "SELECT S FROM C WHERE I = $1";
-const SELECT_LIMITE: &str = "SELECT L FROM C ORDER BY I ASC";
 
 #[derive(serde::Deserialize)]
-pub struct Config {
-    pub pg: deadpool_postgres::Config
-}
+pub struct Config { pub pg: deadpool_postgres::Config }
 
 impl Config {
     pub fn from_env() -> Result<Self, config::ConfigError> {
